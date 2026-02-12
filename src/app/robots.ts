6d89196a73ea53c350878c481d7257ec26f26297
Manel <m.mendez@aguilerasolutions.com>
2026-02-12 14:17:14 +0100

    seo: Update robots.txt with AI bot rules and disallow legal pages

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,47 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+    const commonDisallow = [
+        '/privado/',
+        '/admin/',
+        '/wp-admin/',
+        '/carrito/',
+        '/checkout/',
+        '/api/',
+        '/aviso-legal',
+        '/proteccion-datos',
+        '/politica-cookies',
+        '/declaracion-accesibilidad'
+    ]
+
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/api/'],
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: commonDisallow,
+            },
+            {
+                userAgent: 'GPTBot',
+                allow: '/',
+                disallow: commonDisallow,
+            },
+            {
+                userAgent: 'CC Bot',
+                allow: '/',
+                disallow: commonDisallow,
+            },
+            {
+                userAgent: 'ClaudeBot',
+                allow: '/',
+                disallow: commonDisallow,
+            },
+            {
+                userAgent: 'Google-Extended',
+                allow: '/',
+                disallow: commonDisallow,
+            }
+        ],
         sitemap: 'https://automatizatelo.com/sitemap.xml',
     }
 }
